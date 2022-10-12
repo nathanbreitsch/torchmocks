7de2c48bb369930ef38420f086f801de0ab0fbca
Nathan Breitsch <nathanbreitsch@gmail.com>
2022-10-12 07:26:02 -0400

    Support autograd for conv2d mock

--- torchmocks/nn/conv.py
+++ torchmocks/nn/conv.py
@@ -1,47 +1,84 @@
 import torch
 
 
-class Conv2dMock:
+class MockConvFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight, bias, kernel_size, dilation, padding, stride):
+        batch_size, in_channels, in_height, in_width = x.shape
+        out_channels, in_channels, kernel_height, kernel_width = weight.shape
+        ctx.save_for_backward(x, weight, bias)
+        ctx.save_for_backward(
+            torch.IntTensor(tuple(x.shape)),
+            torch.IntTensor(tuple(weight.shape)),
+            torch.IntTensor(tuple(bias.shape)) if bias is not None else None,
+        )
+
+        out_height = (
+            in_height
+            + 2 * padding[0]
+            - dilation[0] * (kernel_size[0] - 1)
+            - 1
+            + stride[0]
+        ) // stride[0]
+
+        out_width = (
+            in_width
+            + 2 * padding[1]
+            - dilation[1] * (kernel_size[1] - 1)
+            - 1
+            + stride[1]
+        ) // stride[1]
+
+        return torch.zeros((batch_size, out_channels, out_height, out_width))
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x_shape, weight_shape, bias_shape = ctx.saved_tensors
+        x_shape = tuple(x_shape.tolist())
+        weight_shape = tuple(weight_shape.tolist())
+        bias_shape = tuple(bias_shape.tolist()) if bias_shape is not None else None
+
+        grad_input = grad_weight = grad_bias = None
+        if ctx.needs_input_grad[0]:
+            grad_input = torch.zeros(x_shape)
+        if ctx.needs_input_grad[1]:
+            grad_weight = torch.zeros(weight_shape)
+        if bias_shape is not None and ctx.needs_input_grad[2]:
+            grad_bias = torch.zeros(bias_shape)
+        return grad_input, grad_weight, grad_bias, None, None, None, None
+
+
+class MockConvModule:
     def __init__(self, obj):
+        super().__init__()
         self.__class__ = type(
             obj.__class__.__name__, (self.__class__, obj.__class__), {}
         )
         self.__dict__ = obj.__dict__
+        self.weight = obj.weight
+        self.bias = obj.bias
         self.in_channels = obj.in_channels
         self.out_channels = obj.out_channels
         self.kernel_size = obj.kernel_size
         self.stride = obj.stride
         self.padding = obj.padding
         self.dilation = obj.dilation
-        self.mock_gradient_sink = torch.ones(1, requires_grad=True)
 
     def forward(self, x):
-        batch_size, in_channels, in_height, in_width = x.shape
-        assert self.in_channels == in_channels
-        # assert self.device == x.device
-        out_height = (
-            in_height
-            + 2 * self.padding[0]
-            - self.dilation[0] * (self.kernel_size[0] - 1)
-            - 1
-            + self.stride[0]
-        ) // self.stride[0]
-        out_width = (
-            in_width
-            + 2 * self.padding[1]
-            - self.dilation[1] * (self.kernel_size[1] - 1)
-            - 1
-            + self.stride[1]
-        ) // self.stride[1]
-
-        return self.mock_gradient_sink * torch.zeros(
-            (batch_size, self.out_channels, out_height, out_width)
+        return MockConvFunction.apply(
+            x,
+            self.weight,
+            self.bias,
+            self.kernel_size,
+            self.dilation,
+            self.padding,
+            self.stride,
         )
 
 
 mock_dict = {
     # torch.nn.modules.conv.Conv1d,
-    torch.nn.modules.conv.Conv2d: Conv2dMock,
+    torch.nn.modules.conv.Conv2d: MockConvModule,
     # torch.nn.modules.conv.Conv3d,
     # torch.nn.modules.conv.ConvTranspose1d,
     # torch.nn.modules.conv.ConvTranspose2d,
